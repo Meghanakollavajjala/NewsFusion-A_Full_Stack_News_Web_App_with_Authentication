e626126b1f96a28843213f59b674c6d72a299575
Kollavajjala Meghana <146861732+Meghanakollavajjala@users.noreply.github.com>
2025-05-29 15:26:11 +0530

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -14,7 +14,7 @@ function Login() {
     setError(null);
 
     try {
-      const BASE_URL = process.env.REACT_APP_API_URL;
+      const BASE_URL = process.env.REACT_APP_API_BASE_URL;
       const res = await fetch(`${BASE_URL}/api/auth/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
